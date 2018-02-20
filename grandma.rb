68cc473e0adaa7f1d671e665b332c557b5d0dee6
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-20 19:36:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,3 @@
 def speak_to_grandma(greeting)
-if !greeting.upcase
-  puts "HUH? SPEAK UP, SONNY!"
-elsif greeting == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "NO, NOT SINCE 1938"
+
 end
